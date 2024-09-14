2fdcdd8c1b08b7f57b4e0fbd7f0297c00b3a6821
chanmeet01 <comik.chan@gmail.com>
2024-09-14 19:23:20 +0530

    initial commit

---
+++ secure-share/app/_utils/Constant.js
@@ -0,0 +1,3 @@
+export default {
+    desc: 'Secure & Share Govt Documents with Family Members! Keep documents such as mark sheets, PAN cards, passports, and other important documents in digital format!'
+}
